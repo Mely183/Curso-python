cc06aed3e426b8e98b9707479fc395c300e26711
Mely183 <melissa.villarreal183@hotmail.com>
2022-05-01 12:02:07 -0500

    Se utiliza el ciclo for para crear bucles

---
+++ for.py
@@ -0,0 +1,30 @@
+# Recuerda que para hacer comentarios mediante shortcuts debes 
+# seleccionar todo y poner ctrl + K y luego ctrl + C.
+# Pero si quieres descomentar algo lo seleccionas y das click a 
+# ctrol + U
+
+# print (1)
+# print (2)
+# print (3)
+# print (4)
+# print (5)
+# print (6)
+
+# contador = 1
+# print (contador)
+# while contador < 1000:
+#     # contador = contador + 1
+#     contador += 1
+#     print (contador)
+
+
+
+
+# for contador in range (1, 1001):
+#     print (contador)
+
+# a = list (range(1000))
+# print (a)
+
+for i in range (0,10):
+    print (11 * i)  x
